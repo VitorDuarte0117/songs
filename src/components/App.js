9ef0564a4954b29d169e01d3e0968d7c4bd270de
Vitor Duarte <vitorduarte.vds7@gmail.com>
2022-05-16 15:30:03 -0300

    UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,19 @@
+import SongList from "./SongList";
+import SongDetail from "./SongDetail";
+
 function App() {
-    return <div className="App">App</div>;
+    return (
+        <div className="ui container grid">
+            <div className="ui row">
+                <div className="column eight wide">
+                    <SongList />
+                </div>
+                <div className="column eight wide">
+                    <SongDetail />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 import App from "./components/App";
+import reducers from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
         <App />
-    </React.StrictMode>
+    </Provider>,
+    document.querySelector("#root")
 );
